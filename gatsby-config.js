5e6bafe78d1d9901a638cd24974d0f929b3b414a
noureddined123 <69937363+noureddined123@users.noreply.github.com>
2022-11-07 16:27:26 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Netlify CMS modern blog web-app',
+    title: 'Lifestyle Blogs',
     description:
-      'This public repo is an example blog web-app that is built with Gatsby using Netlify CMS. It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Lifestyle Blogs uit Nederland',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
